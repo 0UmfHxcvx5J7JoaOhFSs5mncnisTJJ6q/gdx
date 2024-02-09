b7143b86f7ad7a7dbc5c09727363e5f32cc97ff4
Michaja Pehl <pehl@pik-potsdam.de>
2024-02-09 13:59:30 +0100

    debug

--- R/onLoad.R
+++ R/onLoad.R
@@ -2,8 +2,8 @@
 #' @importFrom withr with_output_sink
 .onLoad <- function(libname, pkgname) {
   tmp <- NULL
-  # with_output_sink(new = textConnection("tmp", "w", local = TRUE),
-  #                  code = {
+  with_output_sink(new = textConnection("tmp", "w", local = TRUE),
+                   code = {
                      path <- strsplit(Sys.getenv("PATH"), .Platform$path.sep,
                                       fixed = TRUE)[[1]]
                      path <- grep("gams", path, ignore.case = TRUE,
@@ -11,6 +11,9 @@
                      # disregard variables on the Windows path
                      path <- grep("%", path, value = TRUE, fixed = TRUE,
                                   invert = TRUE)
+                     # append GAMSROOT (or empty if that does not exist) to make
+                     # sure igdx is called at least once
+                     path <- c(path, Sys.getenv("GAMSROOT"))
 
                      ok <- FALSE
                      for (p in path) {
@@ -18,12 +21,12 @@
                          break
                        }
                      }
-  #                  },
-  #                  append = TRUE)
-  #
-  # if (!ok) {
-  #   # truncate igdx output to 132 characters per line
-  #   tmp <- paste0(strtrim(tmp, 129), c("", "...")[(nchar(tmp) > 132) + 1])
-  #   packageStartupMessage(paste(tmp, collapse = "\n"))
-  # }
+                   },
+                   append = TRUE)
+
+  if (!ok) {
+    # truncate igdx output to 132 characters per line
+    tmp <- paste0(strtrim(tmp, 129), c("", "...")[(nchar(tmp) > 132) + 1])
+    packageStartupMessage(paste(tmp, collapse = "\n"))
+  }
 }
