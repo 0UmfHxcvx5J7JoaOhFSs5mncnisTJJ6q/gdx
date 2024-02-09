bb5fd82dd78eb08d928cd8e932b3fd524c53896c
Michaja Pehl <pehl@pik-potsdam.de>
2024-02-09 16:09:03 +0100

    debug

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,31 +1,24 @@
 #' @importFrom gdxrrw igdx
 #' @importFrom withr with_output_sink
 .onLoad <- function(libname, pkgname) {
-  tmp <- NULL
-                     path <- strsplit(Sys.getenv("PATH"), .Platform$path.sep,
-                                      fixed = TRUE)[[1]]
-                     path <- grep("gams", path, ignore.case = TRUE,
-                                  value = TRUE)
-                     # disregard variables on the Windows path
-                     path <- grep("%", path, value = TRUE, fixed = TRUE,
-                                  invert = TRUE)
-                     # append GAMSROOT (or empty if that does not exist) to make
-                     # sure igdx is called at least once
-                     # path <- c(path, Sys.getenv("GAMSROOT"))
+  path <- strsplit(Sys.getenv("PATH"), .Platform$path.sep, fixed = TRUE)[[1]]
+  path <- grep("gams", path, ignore.case = TRUE, value = TRUE)
+  # disregard variables on the Windows path
+  path <- grep("%", path, value = TRUE, fixed = TRUE, invert = TRUE)
 
-                     ok <- FALSE
-  with_output_sink(new = textConnection("tmp", "w", local = TRUE),
-                   code = {
-                     for (p in path) {
-                       if (isTRUE(ok <- as.logical(igdx(p)))) {
-                         break
-                       }
-                     }
-                   })
+  # append GAMSROOT (or empty if that does not exist) to make sure igdx is
+  # called at least once
+  path <- c(path, Sys.getenv("GAMSROOT"))
+
+  for (p in path) {
+    msg <- capture.output(ok <- as.logical(igdx(p)))
+    if (ok)
+      break
+  }
 
   if (!ok) {
     # truncate igdx output to 132 characters per line
-    tmp <- paste0(strtrim(tmp, 129), c("", "...")[(nchar(tmp) > 132) + 1])
-    packageStartupMessage(paste(tmp, collapse = "\n"))
+    msg <- paste0(strtrim(msg, 129), c("", "...")[(nchar(msg) > 132) + 1])
+    packageStartupMessage(paste(msg, collapse = "\n"))
   }
 }
